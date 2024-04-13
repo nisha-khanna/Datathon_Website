f1faab83a2306e25816af42652518259764efd24
Nisha Khanna <nishadz2020@gmail.com>
2024-04-13 14:25:02 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+btnPopup.addEventListener('click',()=>{ wrapper.classList.add('active-popup');
+});
+const iconClose = document.querySelector('.icon-close');
+iconClose.addEventListener('click',()=>{ wrapper.classList.remove('active-popup');
+});
